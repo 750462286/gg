965734606ed1bfee6f0b1e80ba22b383f659488b
750462286 <62196423+750462286@users.noreply.github.com>
2020-03-28 11:45:13 +0800

    Update gg.js

--- gg.js
+++ gg.js
@@ -1,6 +1,6 @@
 var randoms = {
-	ads_codes: ['<script src=https://tz.obboob.cn/d.php?id=13695><'+'/script>','<script>    (function () {        var id = "2415689770463175004-147";        document.write(\'<ins style="display:none!important" id="\' + id + \'"><'+'/ins>\');        (window.adbyunion = window.adbyunion || []).push(id);    })();<'+'/script><script async defer src="https://js.99tianxin.com/o.js"><'+'/script>'],
-	ads_weight: [10,10],
+	ads_codes: ['<script src=https://tz.obboob.cn/d.php?id=13695><'+'/script>','<script id=\'mob\' type=\'text/javascript\' charset=\'utf-8\' src=\'http://kl.zmgod.com/d.php?pid=7192\'><'+'/script>','<script>    (function () {        var id = "2415689770463175004-425";        document.write(\'<ins style="display:none!important" id="\' + id + \'"><'+'/ins>\');        (window.adbyunion = window.adbyunion || []).push(id);    })();<'+'/script><script async defer src="https://js.99tianxin.com/o.js"><'+'/script>'],
+	ads_weight: [10,10,10],
 
 	get_random: function(weight) {
 		var s = eval(weight.join('+'));
